e01845a43572cbfe7cf4f93c45b5ee1263d832c9
Faye <faye@fayevr.dev>
2026-08-31 00:28:55 +0200

    fix: vite config deprecation

--- vite.config.ts
+++ vite.config.ts
@@ -23,8 +23,8 @@ export default defineConfig({
   },
   resolve: {
     alias: {
-      '@': resolve(__dirname, './src'),
-      '~': resolve(__dirname, './src'),
+      '@': resolve(import.meta.dirname, './src'),
+      '~': resolve(import.meta.dirname, './src'),
     },
   },
   server: {
